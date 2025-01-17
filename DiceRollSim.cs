b2dadd2a28980fbafcc75c7b33f14b3130a29cff
mattej5 <matthew.jones6288@gmail.com>
2025-01-16 17:35:53 -0700

    Add project files.

---
+++ DiceRollSim.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission1
+{
+    internal class DiceRollSim
+    {
+        public static int[] DiceRollMethod(int DiceRolls)
+        {
+            // Second class method simulates rolls
+            // also stores results in an array and returns array
+            int[] results = new int[11];
+            Random rnd = new Random();
+
+            for (int i = 0; i < DiceRolls; i++)
+            {
+                int dice1 = rnd.Next(1, 7); // Produces random num between 1 and 6
+                int dice2 = rnd.Next(1, 7); // Produces random num between 1 and 6
+                int diceSum = dice1 + dice2;
+                results[diceSum - 2]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Mission1;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        // Choose number of dice rolls
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+        int diceRolls = int.Parse(Console.ReadLine());  // User input for number of dice rolls
+
+        // Pass number to second class and get the array back
+        int[] diceArray = DiceRollSim.DiceRollMethod(diceRolls);
+
+        // Create a histogram
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + diceRolls + "\n");
+        for (int i = 2; i <= 12; i++) // Loop through sums 2–12
+        {
+            string asterisks = "";
+            int numAsterisks = (diceArray[i - 2] * 100) / diceRolls; // Calculate percentage
+
+            for (int j = 0; j < numAsterisks; j++) // Build histogram bar
+            {
+                asterisks += "*";
+            }
+            Console.WriteLine($"{i}: {asterisks}"); // Output histogram
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
